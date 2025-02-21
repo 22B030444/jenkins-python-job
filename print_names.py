065df191b667592747344c042d093ef9ba187d42
Zhasmin Suleimenova <122953922+22B030444@users.noreply.github.com>
2025-02-21 21:15:42 +0500

    Update print_names.py

--- print_names.py
+++ print_names.py
@@ -1,2 +1,7 @@
-print("Zhasmin")
-print("Nurkhan")
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python print_names.py <name1> <name2> ...")
+else:
+    for name in sys.argv[1:]:
+        print(name)
